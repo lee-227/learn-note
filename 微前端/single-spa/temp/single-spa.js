a76ea338b3d7df254f9ff3c3d67c7af44ad5b080
Xiao Lee <lee_227@163.com>
2023-06-12 20:14:41 +0800

    feat(微前端): 删除

--- 微前端/single-spa/temp/single-spa.js
+++
@@ -1,4 +0,0 @@
-export { start } from './start.js';
-export { registerApplication } from './applications/apps.js';
-export { navigateToUrl } from './navigation/navigation.js';
-export { addAppToUnload } from './lifecycles/unload.js';
