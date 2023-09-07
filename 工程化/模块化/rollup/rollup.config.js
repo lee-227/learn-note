8184b40e5cd942c67215dde55f309c3fdfc4e258
XiaoLee <lee_227@163.com>
2023-09-07 21:43:13 +0800

    feat(rollup): use

--- 工程化/模块化/rollup/rollup.config.js
+++ 工程化/模块化/rollup/rollup.config.js
@@ -1,12 +1,36 @@
 import json from 'rollup-plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve'; // 按照 node 规则查找 node_modules 中的模块 没有她无法加载第三方模块
+import commonjs from '@rollup/plugin-commonjs'; // 可以加载 commonjs 的模块 
+import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript'; // 支持 ts
+import {terser} from 'rollup-plugin-terser'; // 压缩 js
+import postcss from 'rollup-plugin-postcss'; // 支持 css
+import serve from 'rollup-plugin-serve'; // 启动服务
 export default {
   input: './src/index.js',
   output: {
     // file: 'dist/bundle.js',
     dir: 'dist',
-    format: 'amd',
+    format: 'umd', // amd es iife umd cjs
+    globals: {
+      jquery: '$'
+    }
   },
-  plugins: [json(), resolve(), commonjs()],
+  externals: ['jquery'], // 外部依赖 CDN 引入
+  plugins: [
+    json(),
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: /node_modules/
+    }),
+    typescript(),
+    terser(),
+    postcss(),
+    serve({
+      open: true,
+      port: 8888,
+      contentBase: './dist'
+    })
+  ],
 };
