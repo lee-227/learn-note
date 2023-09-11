58e2ed40d5dcb46686c64f11486339db34ef70d6
XiaoLee <lee_227@163.com>
2023-09-11 22:49:25 +0800

    feat(rollup): use

--- 工程化/模块化/rollup/note.md
+++ 工程化/模块化/rollup/note.md
@@ -1,9 +1,9 @@
 ## 简介
-- 一款类似于webpack的打包工具，但是更为小巧，一个专门的高效的 esmodule 的打包器。
+- 一款类似于 webpack 的模块打包工具，但是更为小巧，一个专门的高效的 esmodule 的打包器。
 
 ## 优点
-- 打包结果更精简
-- 自动 tree-shaking 优化代码
+- webpack 打包非常繁琐，打包体积比较大 而 ES module 打包结果更精简
+- 基于 es module 的自动 tree-shaking , 优化代码体积
 
 ## 缺点
 - 加载非 ESM 的第三方模块需要使用插件
@@ -15,4 +15,5 @@
 - rollup更倾向于开发类库
 - webpack开发应用程序
 
-
+## 插件列表
+- https://github.com/rollup/awesome
--- 工程化/模块化/rollup/rollup.config.js
+++ 工程化/模块化/rollup/rollup.config.js
@@ -1,6 +1,6 @@
 import json from 'rollup-plugin-json';
 import resolve from '@rollup/plugin-node-resolve'; // 按照 node 规则查找 node_modules 中的模块 没有她无法加载第三方模块
-import commonjs from '@rollup/plugin-commonjs'; // 可以加载 commonjs 的模块 
+import commonjs from '@rollup/plugin-commonjs'; // rollup.js编译源码中的模块引用默认只支持 ES6+的模块方式 import/export 需要通过此插件加载 commonjs 的模块 
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript'; // 支持 ts
 import {terser} from 'rollup-plugin-terser'; // 压缩 js
@@ -13,10 +13,10 @@ export default {
     dir: 'dist',
     format: 'umd', // amd es iife umd cjs
     globals: {
-      jquery: '$'
+      jquery: '$' // 告诉 rollup 全局变量 $ 即是 jquery
     }
   },
-  externals: ['jquery'], // 外部依赖 CDN 引入
+  externals: ['jquery'], // 不会打包 引入的此模块
   plugins: [
     json(),
     resolve(),
